1e34cc6c07896a79b3bcc85f5f2275f1ccc503ce
itzrilly <rilwanougarbarilly@gmail.com>
2022-11-10 10:13:16 +0100

    Component Message deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Offer from './pages/Offer';
-import Message from './pages/Message';
+// import Message from './pages/Message';
 import Check from './pages/Check';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login/>} />
           <Route path='/offer' element={<Offer/>} />
-          <Route path='/message' element={<Message/>} />
+          {/* <Route path='/message' element={<Message/>} /> */}
           <Route path='/check' element={<Check/>} />
         </Routes>
       </div>
