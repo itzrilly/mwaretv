9a4ad51f122df6610c04d4468ce6155b8cf4f129
itzrilly <rilwanougarbarilly@gmail.com>
2022-11-10 10:12:09 +0100

    Offer page modified

--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import Alert from '@mui/material/Alert';
 import Button from 'react-bootstrap/Button';
 import './Offer.css';
 import Header from '../components/Header';
@@ -32,38 +33,6 @@ function Offer() {
         if (number) {
             setNumber(number);
         }
-
-        // alert(number);
-        // alert(offerID)
-        // alert('Subscriber number: '+number+' Offer ID: '+offerID);
-
-        // var axios = require('axios');
-        // var data = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:msg="http://oss.huawei.com/business/intf/webservice/subscribe/msg">\n    <soapenv:Header/>\n    <soapenv:Body>\n        <msg:ChangeOptionalOfferRequestMsg>\n            <RequestHeader>\n                <msg:Version>1</msg:Version> <!--DO NOT TOUCH-->\n                <msg:TransactionId>1</msg:TransactionId> <!--DO NOT TOUCH-->\n                <msg:SequenceId>1</msg:SequenceId> <!--DO NOT TOUCH-->\n                <msg:RequestType>Event</msg:RequestType> <!--DO NOT TOUCH-->\n                <msg:ThirdPartyID>156</msg:ThirdPartyID> <!--SOAP Client / Blue Recharge App ID-->\n                <msg:SerialNo>8948487467</msg:SerialNo> <!--Real Transaction ID to be set by SOAP Client-->\n                <msg:Remark>wsr</msg:Remark> <!--Transaction Remark / Comment-->\n            </RequestHeader>\n            <ChangeOptionalOfferRequest>\n                <msg:SubscriberNo>'+number+'</msg:SubscriberNo> <!--Number benefitting from Activation -->\n                <msg:OptionalOffer>\n                    <msg:Id>'+offerID+'</msg:Id> <!--Optional Offer ID-->\n                    <msg:OperationType>1</msg:OperationType> <!--DO NOT TOUCH-->\n                </msg:OptionalOffer>\n                <msg:PrimaryOfferOrderKey>?</msg:PrimaryOfferOrderKey> <!--DO NOT TOUCH-->\n            </ChangeOptionalOfferRequest>\n        </msg:ChangeOptionalOfferRequestMsg>\n    </soapenv:Body>\n</soapenv:Envelope>';
-        
-        // var config = {
-        //     method: 'post',
-        //     url: 'http://192.168.240.7:8280/services/Proxy_SubscribeOptonnal',
-        //     headers: { 
-        //         'Access-Control-Allow-Origin': 'http://localhost:3000/offer',
-        //         'Content-Type': 'text/xml', 
-        //         'SOAPAction': 'changeOptionalOffer'
-        //     },
-        //     data : data
-        // };
-
-        // axios(config).then(function (response) {
-        //     console.log(JSON.stringify(response.data));
-        //     // alert(JSON.stringify(response.data));
-        // }).catch(function (error) {
-        //     console.log(error);
-        //     // alert(error);
-        // });
-
-        // console.log(data);
-
-
-        // alert(offerID);
-
 
     }
 
