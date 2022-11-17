9841c31ba5f41ea6a1590e54467dc299502f2135
itzrilly <rilwanougarbarilly@gmail.com>
2022-11-17 22:30:23 +0100

    Message Component issue resolved with success

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -13,47 +13,32 @@ const Message = () => {
             <Header/>
             
             <div className='msg-content'>
-                {(() => {
-                    if(resultCode == 405000000) {
-                        return(
-                            <div className='msg'>
-                                <p>Offre activée avec succès! Vous allez recevoir vos paramètres de connexion par sms.</p> <br/>
-                                <p>Vous pouvez télécharger l'application en cliquant sur un des liens ci-dessous: </p> <br/>
-                                <p>iOS application: <Link to='/' >Blue VIU</Link></p>
-                                <p>Android application: <Link to='/' >Blue VIU</Link></p> <br/><br/>
-                                <p><Link to='/offer'>Retour en arrière</Link></p>
-                            </div>
-                        )
-                    }else if(resultCode == 405000614) {
-                        return(
-                            <div className='msg-red'>
-                                <p>Le solde de votre compte est insuffisant. Veuillez recharger votre compte.</p> <br/><br/>
-                                <p><Link to='/offer'>Retour en arrière</Link></p>
-                            </div>
-                        )
-                    }else if(resultCode == 405000612) {
-                        return(
-                            <div className='msg-red'>
-                                <p>Le service a été commandé, donc ne peut être ajouté.</p> <br/><br/>
-                                <p><Link to='/offer'>Retour en arrière</Link></p>
-                            </div>
-                        )
-                    }else if(resultCode == 405000615) {
-                        return(
-                            <div className='msg-red'>
-                                <p>Le même ensemble de package facultatif vous permet uniquement d'en sélectionner un.</p> <br/><br/>
-                                <p><Link to='/offer'>Retour en arrière</Link></p>
-                            </div>
-                        )
-                    }else {
-                        return(
-                            <div className='msg-red'>
-                                <p>Echec de l'opération. Veuillez réessayer plus tard.</p> <br/><br/>
-                                <p><Link to='/offer'>Retour en arrière</Link></p>
-                            </div>
-                        )
-                    }
-                })}
+                { resultCode == 405000000 ? (
+                    <div className='msg'>
+                        <p>Offre activée avec succès! Vous allez recevoir vos paramètres de connexion par sms.</p> <br/>
+                        <p>Vous pouvez télécharger l'application en cliquant sur un des liens ci-dessous: </p> <br/>
+                        <p>iOS application: <Link to='/' >Blue VIU</Link></p>
+                        <p>Android application: <Link to='/' >Blue VIU</Link></p> <br/><br/>
+                        <p><Link to='/offer'>Retour en arrière</Link></p>
+                    </div> ) : resultCode == 405000614 ? (
+                    <div className='msg-red'>
+                        <p>Le solde de votre compte est insuffisant. Veuillez recharger votre compte.</p> <br/><br/>
+                        <p><Link to='/offer'>Retour en arrière</Link></p>
+                    </div> ) : resultCode == 405000612 ? (
+                    <div className='msg-red'>
+                        <p>Le service a été commandé, donc ne peut être ajouté.</p> <br/><br/>
+                        <p><Link to='/offer'>Retour en arrière</Link></p>
+                    </div>
+                    ) : resultCode == 405000615 ? (
+                    <div className='msg-red'>
+                        <p>Le même ensemble de package facultatif vous permet uniquement d'en sélectionner un.</p> <br/><br/>
+                        <p><Link to='/offer'>Retour en arrière</Link></p>
+                    </div> ) : (
+                    <div className='msg-red'>
+                        <p>Echec de l'opération. Veuillez réessayer plus tard.</p> <br/><br/>
+                        <p><Link to='/offer'>Retour en arrière</Link></p>
+                    </div>  )
+                }
             </div>
 
             <Footer/>
