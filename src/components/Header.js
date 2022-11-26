296d7e72547e9c1acfa662a19b426d08ff73fbb9
itzrilly <rilwanougarbarilly@gmail.com>
2022-11-26 13:15:36 +0100

    Log out Button added in Header but his functionnality is not implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import LogoutIcon from '@mui/icons-material/Logout';
 import './Header.css';
 
 function Header() {
 
+    // console.log(window.location.pathname); 
+
+    if(window.location.pathname=='/'){
+        console.log('Login page');
+    }
+
     return (
         <div className='header-nav'>
             <Navbar className='navbar'>
-                <Container className='navbar-container justify-content-center'>
+                <Container className={`navbar-container ${window.location.pathname == '/' ? 'justify-content-center' : 'justify-content-between' }`}>
                     <Navbar.Brand href="/" className='navbar-brand justify-content-center'>
                         <img
                             src="/Blue viu_White.png"
@@ -18,6 +25,9 @@ function Header() {
                             alt="mware tv logo"
                         />
                     </Navbar.Brand>
+                    { window.location.pathname=='/' ? '' :
+                        (<div className='log-out-btn'><LogoutIcon/> Déconnexion</div>)
+                    }
                 </Container>
             </Navbar>
         </div>
